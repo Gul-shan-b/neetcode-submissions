6b050a37726ea391cf7c8c99712f4129326c1a30
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-06-17 19:37:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,38 @@
+class MinStack {
+     Stack<Integer> stack;
+     Stack<Integer> minstack;
+
+
+    public MinStack() {
+        stack = new Stack<>();
+        minstack = new Stack<>();
+        
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+        if(minstack.isEmpty()|| value<= minstack.peek()){
+            minstack.push(value);
+        }
+        
+    }
+    
+    public void pop() {
+        if(stack.peek().equals(minstack.peek())){
+            minstack.pop();
+        }
+        stack.pop();
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+        
+    }
+}
+
