12b45c95e87a372f876ee8639b8085921e94bd04
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-06-29 18:44:30 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+        int boat =0;
+        Arrays.sort(people);
+        int left =0;
+        int right=n-1;
+        while(left<=right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+        
+    }
+}
