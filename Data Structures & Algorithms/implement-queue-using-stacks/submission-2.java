cd17da70555db0e96396503382e11ba987b9e197
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-07-07 18:02:04 +0530

    Add: implement-queue-using-stacks - submission-2

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-2.java
@@ -0,0 +1,31 @@
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if(stack2.empty()){
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty()&& stack2.isEmpty();
+    }
+}
