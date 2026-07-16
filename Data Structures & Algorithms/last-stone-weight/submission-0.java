a9c44209ce32d0d0d2cb57d30cb20ae573c8d29d
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-07-16 19:00:47 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+
+       
+        for(int i:stones){
+            heap.add(i);
+                 
+            
+        }
+        while(heap.size()>1){
+            int x = heap.poll();
+            int y = heap.poll();
+            if(x!=y){
+                heap.add(x-y);
+            }
+            if(heap.isEmpty()){
+                return 0;
+            }
+        }
+        return heap.peek();
+        
+    }
+}
