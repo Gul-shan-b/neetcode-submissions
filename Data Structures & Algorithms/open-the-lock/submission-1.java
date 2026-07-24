9b59d7847c484752c476fc5da00b4b9a0555f244
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-07-24 17:27:02 +0530

    Add: open-the-lock - submission-1

---
+++ Data Structures & Algorithms/open-the-lock/submission-1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+
+        if(visited.contains("0000")) return -1;
+
+        Queue<String> queue = new LinkedList<>();
+        visited.add("0000");
+        queue.add("0000");
+        int turn =0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i =0; i<size; i++){
+                String lock = queue.poll();
+                if(lock.equals(target)) return turn;
+
+                for(String next:children(lock)){
+                    if(!visited.contains(next)){
+                        queue.add(next);
+                        visited.add(next);
+                    }
+                }
+            }
+            turn ++;
+
+        }
+        return -1;
+        
+    }
+    private List<String> children(String lock){
+        List<String> res = new ArrayList<>();
+        for(int i =0; i<4; i++){
+            char[] arr = lock.toCharArray();
+            arr[i] = (char)(((arr[i]-'0'+1)%10)+'0');
+            res.add(new String(arr));
+            arr = lock.toCharArray();
+            arr[i] = (char)(((arr[i]-'0'-1+10)%10)+'0');
+            res.add(new String(arr));
+
+        }
+        return  res;
+    }
+}
