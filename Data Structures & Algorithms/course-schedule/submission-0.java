91b6cd4466169ace7364a4877e76754433660cde
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-07-24 18:29:28 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        List<List<Integer>> graph = new ArrayList<>();
+        int [] indegree = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] pre: prerequisites){
+            graph.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i =0; i<numCourses; i++){
+            if(indegree[i]==0){
+                queue.add(i);
+            }
+        }
+        int count =0;
+        while(!queue.isEmpty()){
+            
+            int cur = queue.poll();
+            count++;
+
+            for(int next:graph.get(cur)){
+                indegree[next]--;
+
+                if(indegree[next]== 0) queue.add(next);
+            }
+
+        }
+        return count==numCourses;
+
+
+        
+    }
+}
