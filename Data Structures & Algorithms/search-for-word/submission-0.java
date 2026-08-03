23f47a39995b6b426d6eee3f76f82ca67fcbc574
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-08-03 17:51:32 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m =board.length;
+        int n = board[0].length;
+        for(int r = 0; r<m; r++){
+            for(int c= 0; c<n; c++){
+                if(word.charAt(0)==board[r][c]){
+                    boolean found = dfs(board, word, r, c, 0);
+                    if(found) return true;
+                }
+            }
+        }
+        return false;   
+    }
+    private boolean dfs(char[][]board, String word, int r, int c, int wordindex){
+        if(wordindex == word.length()){
+            return true;
+        }
+
+        if(r<0 || c<0 || r >= board.length || c >= board[0].length){
+            return false;
+        }
+        if(board[r][c]==' ' || board[r][c] != word.charAt(wordindex)){
+            return false;
+        }
+        char ch = board[r][c];
+        board[r][c] = ' ';
+        if(dfs(board,word,r+1,c,wordindex+1)|| 
+        dfs(board,word,r-1,c,wordindex+1)|| 
+        dfs(board,word,r,c-1,wordindex+1)||
+        dfs(board,word,r,c+1,wordindex+1)){
+            return true;
+        }
+        board[r][c] = ch;
+
+        return false;
+
+    }
+    
+}
