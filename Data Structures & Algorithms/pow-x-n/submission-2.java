2fee1dea76d0db5d8fd13461f472c07b58c02ff5
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-08-09 20:58:23 +0530

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        double res = 1;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                res *= x;
+            }
+
+            x *= x;
+            N /= 2;
+        }
+
+        return res;
+    }
+}
