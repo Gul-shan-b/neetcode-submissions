b1fc69691b18c87830db7685dc686bd9313758c8
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-08-10 19:27:21 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int min =1;
+        int max = 0;
+        for(int pile:piles){
+            max= Math.max(max, pile);
+        }
+        while(min<max){
+            int mid = min+(max-min)/2;
+            if(canieat(piles,h,mid)){
+                max = mid;
+            }
+            else{
+                min = mid+1;
+            }
+        }
+        return min;
+        
+    }
+    private boolean canieat(int[] piles, int h, int speed){
+        int hours =0;
+        for(int pile:piles){
+            hours += (int)Math.ceil((double)pile/speed);
+        }
+        return hours <= h;
+    }
+}
