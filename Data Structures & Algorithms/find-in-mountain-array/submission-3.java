0f640ab424f5ee3c67608372d4ca92b134225206
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-08-12 19:36:48 +0530

    Add: find-in-mountain-array - submission-3

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-3.java
@@ -0,0 +1,74 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+
+        int n = mountainArr.length();
+
+        // Find peak
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        int peak = left;
+
+        // Search increasing part
+        left = 0;
+        right = peak;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int value = mountainArr.get(mid);
+
+            if (value == target) {
+                return mid;
+            }
+
+            if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // Search decreasing part
+        left = peak + 1;
+        right = n - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int value = mountainArr.get(mid);
+
+            if (value == target) {
+                return mid;
+            }
+
+            if (value > target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
