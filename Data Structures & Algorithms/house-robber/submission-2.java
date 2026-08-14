9908998b1f4828cc99b7d8b7f64e8a10632ca721
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-08-14 19:22:21 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }
+        int prev1 = nums[0];
+        int prev2 = 0;
+        int ans =0;
+        for(int i =2; i<n+1; i++){
+            int pick = nums[i-1] + prev2;
+            int notpick = 0+ prev1;
+            ans = Math.max(pick, notpick);
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return ans ;
+        
+    }
+}
