d86f90b833f986f3fc0a2ab133445bcae88e04a8
Gulshan Banu <gulshanbanu.cd23@bmsce.ac.in>
2026-08-31 19:18:21 +0530

    Add: stone-game-ii - submission-0

---
+++ Data Structures & Algorithms/stone-game-ii/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    int n;
+    int[][] dp;
+    int[] suffix;
+
+    public int stoneGameII(int[] piles) {
+        n = piles.length;
+        dp = new int[n][n + 1];
+        suffix = new int[n + 1];
+
+        // Suffix sum
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        return solve(0, 1);
+    }
+
+    private int solve(int i, int M) {
+
+        if (i >= n) {
+            return 0;
+        }
+
+        if (dp[i][M] != 0) {
+            return dp[i][M];
+        }
+
+        int maxStones = 0;
+
+        for (int X = 1; X <= 2 * M && i + X <= n; X++) {
+
+            int opponent = solve(i + X, Math.max(M, X));
+
+            int current = suffix[i] - opponent;
+
+            maxStones = Math.max(maxStones, current);
+        }
+
+        return dp[i][M] = maxStones;
+    }
+}
